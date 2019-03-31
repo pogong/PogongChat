9241eb07aa2ee581ba1e488b5d146961d2cf927f
pogong <564917199@qq.com>
2019-03-31 16:07:19 +0800

    add timestamp setting;

--- PogongChat/src/main/java/com/pogong/PogongChat/AnyController.java
+++ PogongChat/src/main/java/com/pogong/PogongChat/AnyController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,9 +14,12 @@ public class AnyController {
 
     @GetMapping
     public Map<String,Object> sayHelloWorld(){
-        System.out.println("hello world!");
+        System.out.println("hello world!5566");
         Map<String,Object> result = new HashMap<>();
-        result.put("pg_key","pg_value");
+        result.put("pg_key1111","pg_value1111");
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2016,Calendar.OCTOBER,2,0,0);
+        result.put("pg_key2",calendar.getTime());
         return result;
     }
 
