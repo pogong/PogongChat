251a5ffaa538f56bcfb24fb6293009b484a0c858
pogong <564917199@qq.com>
2019-03-31 16:26:22 +0800

    add log tool;

--- PogongChat/src/main/java/com/pogong/PogongChat/AnyController.java
+++ PogongChat/src/main/java/com/pogong/PogongChat/AnyController.java
@@ -1,5 +1,7 @@
 package com.pogong.PogongChat;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,16 @@ import java.util.Map;
 @RequestMapping("/sayHelloWorld")
 public class AnyController {
 
+    private static final Log log = LogFactory.getLog(AnyController.class);
+
     @GetMapping
     public Map<String,Object> sayHelloWorld(){
-        System.out.println("hello world! pg see");
+
+        if (log.isTraceEnabled()){
+            log.trace("sayHelloWorld 被调用了");
+        }
+
+        System.out.println("System print hello world!");
         Map<String,Object> result = new HashMap<>();
         result.put("pg_key1111","pg_value1111");
         Calendar calendar = Calendar.getInstance();
