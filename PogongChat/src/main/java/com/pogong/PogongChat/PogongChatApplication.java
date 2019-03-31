c47290d9c4c3e70a80e3deb96586ed8999839885
pogong <564917199@qq.com>
2019-03-31 10:07:46 +0800

    add init project from start.spring.io;

---
+++ PogongChat/src/main/java/com/pogong/PogongChat/PogongChatApplication.java
@@ -0,0 +1,13 @@
+package com.pogong.PogongChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PogongChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PogongChatApplication.class, args);
+	}
+
+}
---
+++ PogongChat/src/test/java/com/pogong/PogongChat/PogongChatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pogong.PogongChat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PogongChatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
