f311f3e06059293e297b9fbf2d8f8f7bfec97851
pogong <564917199@qq.com>
2019-04-03 10:28:03 +0800

    add connectDB;

--- PogongChat/src/main/java/com/pogong/PogongChat/AnyController.java
+++ PogongChat/src/main/java/com/pogong/PogongChat/AnyController.java
@@ -2,21 +2,22 @@ package com.pogong.PogongChat;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
-@RequestMapping("/sayHelloWorld")
+@RequestMapping("/any")
 public class AnyController {
 
     private static final Log log = LogFactory.getLog(AnyController.class);
 
-    @GetMapping
+    @GetMapping("/sayHelloWorld")
     public Map<String,Object> sayHelloWorld(){
 
         if (log.isTraceEnabled()){
@@ -26,10 +27,35 @@ public class AnyController {
         System.out.println("System print hello world!");
         Map<String,Object> result = new HashMap<>();
         result.put("pg_key1111","pg_value1111");
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2016,Calendar.OCTOBER,2,0,0);
-        result.put("pg_key2",calendar.getTime());
         return result;
     }
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/connectDB")
+    public int connectDB(){
+        String sql = "select * from tv_series";
+        List<Map<String, Object>> list =  jdbcTemplate.queryForList(sql);
+        for (Map<String, Object> map : list) {
+
+            System.out.println("pg see begin");
+            System.out.println(map.getClass());
+            System.out.println(map);
+            System.out.println("pg see end");
+
+            Set<Map.Entry<String, Object>> entries = map.entrySet( );
+            if(entries != null) {
+                Iterator<Map.Entry<String, Object>> iterator = entries.iterator( );
+                while(iterator.hasNext( )) {
+                    Map.Entry<String, Object> entry =(Map.Entry<String, Object>) iterator.next( );
+                    Object key = entry.getKey( );
+                    Object value = entry.getValue();
+                    System.out.println("pg see>"+key+":"+value);
+                }
+            }
+
+        }
+        return 1;
+    }
 }
